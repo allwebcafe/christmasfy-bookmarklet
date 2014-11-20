7d578272b59ece0532e2ce9790159a3b61bd4cf0
Brandon Morse <bmorse@allwebcafe.com>
2014-11-20 11:12:12 -0500

    Adds New Fonts Adds Adobe Edge Animation for the "How To" Removes Foundation 4.3.2 Adds Foundation 5

--- public/js/christmasfy-bookmarklet.js
+++ public/js/christmasfy-bookmarklet.js
@@ -1,4 +1,3 @@
-/* Insert Completed Christmasfy Your Website Bookmarklet Code Here */
 javascript:(function(e,a,g,h,f,c,b,d){if(!(f=e.jQuery)||g>f.fn.jquery||h(f)){c=a.createElement('script');c.type='text/javascript';c.src='//ajax.googleapis.com/ajax/libs/jquery/'+g+'/jquery.min.js';c.onload=c.onreadystatechange=function(){if(!b&&(!(d=this.readyState)||d=='loaded'||d=='complete')){h((f=e.jQuery).noConflict(1),b=1);f(c).remove()}};a.body.appendChild(c)}})(window,document,'1.8',function($,L){  
 
 
